140814775eb8796db739a9a2c0e9a8c450343a09
lucpizz <Lucpizz@gmail.com>
2021-03-30 20:15:02 -0400

    created product model

--- models/productModel.js
+++ models/productModel.js
@@ -1,12 +1,7 @@
+//Product Schema
 const Mongoose = require('mongoose');
-const slug = require('mongoose-slug-generator');
 const { Schema } = Mongoose;
-const options = {
-	separator: '-',
-	lang: 'en',
-	truncate: 120,
-};
-Mongoose.plugin(slug, options);
+
 // Product Schema
 const ProductSchema = new Schema({
 	sku: {
@@ -15,11 +10,6 @@ const ProductSchema = new Schema({
 	name: {
 		type: String,
 		trim: true,
-	},
-	slug: {
-		type: String,
-		slug: 'name',
-		unique: true,
 	},
 	imageUrl: {
 		type: String,
