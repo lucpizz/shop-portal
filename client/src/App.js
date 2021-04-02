6488a529b6fc88703b92b53b30dc44c91d1512bc
Jeremy Rice <jdavidrice@gmail.com>
2021-04-01 23:37:01 -0400

    Add test button to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Button from '@material-ui/core/Button';
+// import Menu from '@material-ui/core/Menu';
+// import MenuItem from '@material-ui/core/MenuItem';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
+        <Button variant="contained" color="default">
+          Hi Mom!
+        </Button>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -22,3 +29,20 @@ function App() {
 }
 
 export default App;
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Button from '@material-ui/core/Button';
+
+// function App() {
+//   return (
+//     <Button variant="contained" color="primary">
+//       Hello World
+//     </Button>
+//   );
+// }
+
+// ReactDOM.render(<App />, document.querySelector('#app'));
+
+// export default App;
