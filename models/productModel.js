d572ff59ca53b372488c01ff6c03b1cf7d98d3d0
lucpizz <Lucpizz@gmail.com>
2021-04-03 13:45:29 -0400

    adding productModel schema

---
+++ models/productModel.js
@@ -0,0 +1,49 @@
+//Product Schema
+const Mongoose = require('mongoose');
+const { Schema } = Mongoose;
+
+// Product Schema
+const ProductSchema = new Schema({
+  sku: {
+    type: String,
+  },
+  name: {
+    type: String,
+    trim: true,
+  },
+  imageUrl: {
+    type: String,
+  },
+  imageKey: {
+    type: String,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  quantity: {
+    type: Number,
+  },
+  price: {
+    type: Number,
+  },
+  taxable: {
+    type: Boolean,
+    default: false,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  brand: {
+    type: Schema.Types.ObjectId,
+    ref: 'Brand',
+    default: null,
+  },
+  updated: Date,
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = Mongoose.model('Product', ProductSchema);
