ce1aee5dde2c380d443afefd5d9b617d0a90b127
Jeremy Rice <jdavidrice@gmail.com>
2021-04-03 23:53:47 -0400

    Add SignIn test file

---
+++ client/src/tests/SignIn.test.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SignIn from '../pages/SignIn/SignIn';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<SignIn />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
