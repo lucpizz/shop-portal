da5c8fcd5ebb728dd3166b03c03ef0be36a68ea5
Jeremy Rice <jdavidrice@gmail.com>
2021-04-04 00:23:19 -0400

    Remove unneeded semicolons from test files

--- client/src/tests/SignIn.test.js
+++ client/src/tests/SignIn.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SignIn from '../pages/SignIn/SignIn';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SignIn from '../pages/SignIn/SignIn'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
