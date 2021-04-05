245e757eb9cf75bcf024452d1616b7bd952eff88
David Rodriguez <david@bsdadm.com>
2021-04-04 23:00:20 -0400

    added slug generator

--- models/brandModel.js
+++ models/brandModel.js
@@ -1,5 +1,13 @@
 const Mongoose = require('mongoose');
 const { Schema } = Mongoose;
+const slug = require('mongoose-slug-generator');
+const options = {
+  separator: '-',
+  lang: 'en',
+  truncate: 120,
+};
+
+Mongoose.plugin(slug, options);
 
 //Brand Schema
 const BrandSchema = new Schema({
