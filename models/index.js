48fa7958aac493ce9183428191654d8f4e6a6304
Delph <delph_teb@hotmail.com>
2021-04-06 18:42:19 -0400

    fix typo

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  Address: require('./addressyModel'),
+  Address: require('./addressModel'),
   Brand: require('./brandModel'),
   Cart: require('./cartModel'),
   Category: require('./categoryModel'),
