9bb1cc52864008226147c9c1abdfc33177fd74c7
Jeremy Rice <jdavidrice@gmail.com>
2021-04-07 11:56:31 -0400

    Add SignUp page template

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import { CssBaseline } from '@material-ui/core';
 import Navigation from './components/Navigation/Navigation';
 import SignIn from './pages/SignIn/SignIn';
+import SignUp from './pages/SignUp/SignUp';
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
 
       <Switch>
         <Route exact from='/login' render={(props) => <SignIn {...props} />} />
+      </Switch>
+      <Switch>
+        <Route exact from='/signup' render={(props) => <SignUp {...props} />} />
       </Switch>
     </>
   );
