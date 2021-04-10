7b7120c513915ea0191f2cbfb404b26c75285402
lucpizz <Lucpizz@gmail.com>
2021-04-10 18:41:00 -0400

    added controller for isReviewed feature

---
+++ controllers/isReviewedController.js
@@ -0,0 +1,42 @@
+const db = require('../models/isReviewedModel');
+
+// Defining methods for the postsController
+module.exports = {
+  findAll: function (req, res) {
+    db.find(req.query)
+      .sort({ created: -1 })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  findById: function (req, res) {
+    db.findById(req.params.id)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  findByTotalStars: function (req, res) {
+    db.findByOne({ sku: req.params.totalStars })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  findByName: function (req, res) {
+    db.findByOne({ name: req.params.name })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  create: function (req, res) {
+    db.create(req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  update: function (req, res) {
+    db.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  remove: function (req, res) {
+    db.findById({ _id: req.params.id })
+      .then((dbModel) => dbModel.remove())
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+};
