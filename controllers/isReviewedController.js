380eae5aa4b003dca0f3e148d6971c001f4c9821
lucpizz <Lucpizz@gmail.com>
2021-04-11 14:01:03 -0400

    changed query for user adding the toString method

--- controllers/isReviewedController.js
+++ controllers/isReviewedController.js
@@ -3,7 +3,7 @@ const db = require('../models/isReviewedModel');
 // Defining methods for the postsController
 module.exports = {
   findAll: function (req, res) {
-    db.find(req.query)
+    db.find(req.query.toString())
       .sort({ created: -1 })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
