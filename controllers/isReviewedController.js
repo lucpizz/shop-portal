f53cd1327f9213381e23b86cc116b480c3689a74
lucpizz <Lucpizz@gmail.com>
2021-04-12 19:52:46 -0400

    testing express-validator for mitigating api call issue

--- controllers/isReviewedController.js
+++ controllers/isReviewedController.js
@@ -1,7 +1,9 @@
 const db = require('../models/isReviewedModel');
+//const { body, validateResult } = require('express-validator');
 
 // Defining methods for the postsController
 module.exports = {
+
   findAll: function (req, res) {
     // let query = { name: req.query.name };
     db.find()
@@ -25,6 +27,9 @@ module.exports = {
       .catch((err) => res.status(422).json(err));
   },
   create: function (req, res) {
+
+    const {query} = requre('express-validator')
+
     let query = {
       name: req.body.name,
       imageUrl: req.body.imageUrl,
