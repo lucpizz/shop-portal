a8ad3d3e1ca490f7bca15828b403af210efa50d9
Money Penny <pennyquirino@gmail.com>
2021-04-15 19:38:53 -0400

    added masonry grid

--- client/src/pages/Shop/Shop.jsx
+++ client/src/pages/Shop/Shop.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ProductsList from '../../__mock__/ProductsList';
+import Masonry from 'react-masonry-css';
+import useStyles from './styles';
 import {
   CardMedia,
   CardHeader,
@@ -11,29 +13,43 @@ import {
 } from '@material-ui/core';
 
 const Shop = () => {
-  //   const {title, price, description, imgUrl } = ProductsList;
+  const classes = useStyles();
+  const breakPoints = {
+    default: 3,
+    1100: 3,
+    700: 2,
+    500: 1,
+  };
   return (
-    <>
+    <Masonry
+      breakpointCols={breakPoints}
+      className={classes.myMasonryGrid}
+      columnClassName={classes.myMasonryGridColumn}>
       {ProductsList
         ? ProductsList.map((product, key) => {
             return (
-              <Card key={key}>
-                <CardHeader title={product.title} subheader={product.price} />
-                <CardMedia style={{ height: '200px' }} image={product.imgUrl} />
-                <CardContent>
-                  <Typography variant='body2' component='p'>
-                    {product.description}
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size='small'>ADD TO CART</Button>
-                  <Button size='small'>DETAILS</Button>
-                </CardActions>
-              </Card>
+              <div key={key} className={classes.myMasonryGridColumnDiv}>
+                <Card>
+                  <CardHeader title={product.title} subheader={product.price} />
+                  <CardMedia
+                    style={{ height: '200px' }}
+                    image={product.imgUrl}
+                  />
+                  <CardContent>
+                    <Typography variant='body2' component='p'>
+                      {product.description}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size='small'>ADD TO CART</Button>
+                    <Button size='small'>DETAILS</Button>
+                  </CardActions>
+                </Card>
+              </div>
             );
           })
         : null}
-    </>
+    </Masonry>
   );
 };
 
