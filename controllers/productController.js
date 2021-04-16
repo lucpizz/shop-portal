8aa2301095c26b7f5437fa20b9e21e21a290ec27
lucpizz <Lucpizz@gmail.com>
2021-04-15 23:01:57 -0400

    fixed bug with findAll query

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,28 +3,52 @@ const db = require('../models/productModel');
 // Defining methods for the postsController
 module.exports = {
   findAll: function (req, res) {
-    db.find(req.query)
+    db.find()
       .sort({ created: -1 })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
   findById: function (req, res) {
-    db.findById(req.params.id)
+    let findId = new db({
+      id: req.parmas.id,
+    });
+    db.findById(findId)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
   findBySku: function (req, res) {
-    db.findOne({ sku: req.params.sku })
+    let findSku = new db({
+      sku: req.parmas.sku,
+    });
+    db.findOne(findSku)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
   findByName: function (req, res) {
-    db.findOne({ name: req.params.name })
+    let findName = new db({
+      name: req.parmas.name,
+    });
+    db.findOne(findName)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
   create: function (req, res) {
-    db.create(req.body)
+    let createProduct = new db({
+      sku: req.body.sku,
+      slug: req.body.slug,
+      name: req.body.name,
+      imageUrl: req.body.imageUrl,
+      imageKey: req.body.imageKey,
+      description: req.body.description,
+      quantity: req.body.quantity,
+      price: req.body.price,
+      taxable: req.body.taxable,
+      isActive: req.body.isActive,
+      brand: req.body.brand,
+      created: req.body.created,
+      updated: req.body.updated,
+    });
+    db.create(createProduct)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
