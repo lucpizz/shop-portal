39a7fffd8042a1d2bce549b7816c2aba1b9e4644
Delph <delph_teb@hotmail.com>
2021-04-15 20:37:36 -0400

    fix typo

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -2,33 +2,29 @@ require('dotenv').config();
 const stripe = require('stripe')(process.env.SECRET_KEY);
 
 const calculateOrderAmount = () => {
-    // Replace this constant with a calculation of the order's amount
-    // Calculate the order total on the server to prevent
-    // people from directly manipulating the amount on the client
-    return 1400;
-  };
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return 1400;
+};
 
 // Defining methods for the postsController
 module.exports = {
-  purchase:  async (req, res) => {
+  purchase: async (req, res) => {
     const { items } = req.body;
     // Create a PaymentIntent with the order amount and currency
     const paymentIntent = await stripe.paymentIntents.create({
       amount: calculateOrderAmount(items),
-      currency: "usd"
+      currency: 'usd',
     });
-  res.send({
-      clientSecret: paymentIntent.client_secret
+    res.send({
+      clientSecret: paymentIntent.client_secret,
     });
   },
   // get: (req,res)=> {
   //   res.json({msg:"test" });
-  }
-
 };
 
-  
-  
 //   server.post("/payment", async (req, res) => {
 //     const { items } = req.body;
 //     // Create a PaymentIntent with the order amount and currency
