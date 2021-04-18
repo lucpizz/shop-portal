ced8c1c155716dc107475017c5fe05798f042172
Delph <delph_teb@hotmail.com>
2021-04-18 02:57:21 -0400

    add parameters for aggregate

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,7 +3,47 @@ const db = require('../models/productModel');
 // Defining methods for the postsController
 module.exports = {
   findAll: function (req, res) {
-    db.find(req.query)
+    db.aggregate([
+      {
+        $lookup: {
+          from: 'reviews',
+          localField: 'isReviewed',
+          foreignField: '_id',
+          as: 'reviews',
+        },
+      },
+      {
+        $unwind: {
+          path: '$reviews',
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $group: {
+          _id: '$_id',
+          averageStars: {
+            $avg: '$reviews.totalStars',
+          },
+          sku: { $first: '$sku' },
+          slug: { $first: '$slug' },
+          name: { $first: '$name' },
+          imageUrl: { $first: '$imageUrl' },
+          imageKey: { $first: '$imageKey' },
+          description: { $first: '$description' },
+          price: { $first: '$price' },
+          brand: { $first: '$brand' },
+          isActive: { $first: '$isActive' },
+          reviews: {
+            $push: {
+              title: '$reviews.name',
+              totalStars: '$reviews.totalStars',
+              description: '$reviews.reviewBody',
+              isActive: '$reviews.isActive',
+            },
+          },
+        },
+      },
+    ])
       .sort({ created: -1 })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
