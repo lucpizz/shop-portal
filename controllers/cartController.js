318b6310a7bba029c476471ce86fe19dcb4279e6
Delph <delph_teb@hotmail.com>
2021-04-19 11:23:23 -0400

    fix controller cart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,6 +1,6 @@
 const db = require('../models/cartModel');
 
-// Defining methods for the postsController
+// Defining methods for the cartController
 module.exports = {
   findAll: function (req, res) {
     db.find()
@@ -10,63 +10,101 @@ module.exports = {
       .catch((err) => res.status(422).json(err));
   },
   findById: function (req, res) {
-    let findId = new db({
+    let findId = new db({   // EXTRA LINES because of SONAR errors
       id: req.params.id,
     });
-    db.findById(findId)
+    db.findById({_id: findId.id})
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
-  findByProduct: function (req, res) {
-    let findProduct = new db({
-      product: req.params.product,
-    });
-    db.findOne(findProduct)
-      .then((dbModel) => res.json(dbModel))
-      .catch((err) => res.status(422).json(err));
-  },
-  findByStatus: function (req, res) {
-    let findStatus = new db({
-      status: req.params.status,
-    });
-    db.findOne(findStatus)
-      .populate('products.product products.product.brand')
-      .then((dbModel) => res.json(dbModel))
-      .catch((err) => res.status(422).json(err));
-  },
+  
   findByUserandStatus: function (req, res) {
-    let findUserStatus = new db({
+    let findUserStatus = new db({  // EXTRA LINES because of SONAR errors
       user: req.params.user,
       status: req.params.status,
-    });
-    db.find(findUserStatus)
+    }); 
+    db.find({user: findUserStatus.user, status: findUserStatus.status})
       .populate('products.product products.product.brand')
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
-  create: function (req, res) {
-    let createCart = new db({
+  create: function (req, res) { 
+    let createCart = new db({   // EXTRA LINES because of SONAR errors
       product: req.params.product,
       quantity: req.params.quantity,
       totalPrice: req.params.totalPrice,
       priceWithTax: req.params.priceWithTax,
       status: req.params.status,
     });
-    db.create(createCart)
+    db.create({
+      product: createCart.product,
+      quantity: createCart.quantity,
+      totalPrice: createCart.totalPrice,
+      priceWithTax: createCart.priceWithTax,
+      status: createCart.status
+    })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
   update: function (req, res) {
-    let updateProduct = new db({ _id: req.params.id }, req.body);
-    db.findOneAndUpdate(updateProduct)
+    db.findOneAndUpdate({ _id: req.params.id }, req.body)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
+  
   remove: function (req, res) {
-    let removeProduct = new db({ _id: req.params.id });
-    db.findById(removeProduct)
+    let removeProduct = new db({ id: req.params.id });
+    console.log(removeProduct);
+    db.findById({_id: removeProduct.id})
       .then((dbModel) => dbModel.remove())
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
+  //  *****UNUSED*****
+  // findByIdandProduct: function (req, res) {
+  //   db.find({ _id: req.params.id, 'products._id': req.params.product })
+  //     .then((dbModel) => res.json(dbModel))
+  //     .catch((err) => res.status(422).json(err));
+  // },
+  // findByProduct: function (req, res) {
+  //   let findProduct = new db({
+  //     'products._id': req.params.product,
+  //   });
+  //   db.findOne(findProduct)
+  //     .populate('products.product products.product.brand')
+  //     .then((dbModel) => res.json(dbModel))
+  //     .catch((err) => res.status(422).json(err));
+  // },
+  // findByStatus: function (req, res) {
+  //   let findStatus = new db({
+  //     status: req.params.status,
+  //   });
+  //   db.findOne(findStatus)
+  //     .populate('products.product products.product.brand')
+  //     .then((dbModel) => res.json(dbModel))
+  //     .catch((err) => res.status(422).json(err));
+  // },
+  // updateAProduct: function (req, res) {
+  //   let updateProduct = new db(
+  //     { _id: req.params.id, 'products._id': req.params.product },
+  //     req.body
+  //   );
+  //   db.findOneAndUpdate(updateProduct)
+  //     .then((dbModel) => res.json(dbModel))
+  //     .catch((err) => res.status(422).json(err));
+  // },  
+  // removeAProduct: function (req, res) {
+  //   let removeProduct = new db({
+  //     _id: req.params.id,
+  //     'products._id': req.params.product,
+  //   });
+  //   db.findOneAndRemove(removeProduct)
+  //     // .then((dbModel) => dbModel.remove())
+  //     .then((dbModel) => {
+  //       console.log(res.json(dbModel)); // FOR TESTING
+  //       res.json(dbModel);
+  //     })
+  //     .catch((err) => res.status(422).json(err));
+  // },
+  //  *****END OF UNUSED*****
 };
