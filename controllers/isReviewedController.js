27de7485a0a9a766345c120df6216a3d6752c692
lucpizz <Lucpizz@gmail.com>
2021-04-19 14:11:43 -0400

    updated isReviewedController configuration

--- controllers/isReviewedController.js
+++ controllers/isReviewedController.js
@@ -4,7 +4,7 @@ const db = require('../models/isReviewedModel');
 
 module.exports = {
   findAll: function (req, res) {
-    db.find()
+    db.find(req.query)
       .sort({ created: -1 })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
