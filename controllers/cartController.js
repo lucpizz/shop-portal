bf73703525d370edc231c68379ff67af09740334
Delph <delph_teb@hotmail.com>
2021-04-19 01:10:32 -0400

    cleanup back from unused code

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -10,60 +10,91 @@ module.exports = {
       .catch((err) => res.status(422).json(err));
   },
   findById: function (req, res) {
-    db.findById(req.params.id)
+    let findId = new db({
+      id: req.params.id,
+    });
+    db.findById(findId)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
-  findByIdandProduct: function (req, res) {
-    db.find({ _id: req.params.id, 'products._id': req.params.product })
-      .then((dbModel) => res.json(dbModel))
-      .catch((err) => res.status(422).json(err));
-  },
-  findByProduct: function (req, res) {
-    db.findOne({ 'products._id': req.params.product })
-      .populate('products.product products.product.brand')
-      .then((dbModel) => res.json(dbModel))
-      .catch((err) => res.status(422).json(err));
-  },
-  findByStatus: function (req, res) {
-    db.findOne({ status: req.params.status })
-      .populate('products.product products.product.brand')
-      .then((dbModel) => res.json(dbModel))
-      .catch((err) => res.status(422).json(err));
-  },
+  // findByIdandProduct: function (req, res) {
+  //   db.find({ _id: req.params.id, 'products._id': req.params.product })
+  //     .then((dbModel) => res.json(dbModel))
+  //     .catch((err) => res.status(422).json(err));
+  // },
+  // findByProduct: function (req, res) {
+  //   let findProduct = new db({
+  //     'products._id': req.params.product,
+  //   });
+  //   db.findOne(findProduct)
+  //     .populate('products.product products.product.brand')
+  //     .then((dbModel) => res.json(dbModel))
+  //     .catch((err) => res.status(422).json(err));
+  // },
+  // findByStatus: function (req, res) {
+  //   let findStatus = new db({
+  //     status: req.params.status,
+  //   });
+  //   db.findOne(findStatus)
+  //     .populate('products.product products.product.brand')
+  //     .then((dbModel) => res.json(dbModel))
+  //     .catch((err) => res.status(422).json(err));
+  // },
   findByUserandStatus: function (req, res) {
-    db.find({ user: req.params.user, status: req.params.status })
+    let findUserStatus = new db({
+      user: req.params.user,
+      status: req.params.status,
+    });
+    db.find(findUserStatus)
       .populate('products.product products.product.brand')
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
   create: function (req, res) {
-    db.create(req.body)
+    let createCart = new db({
+      product: req.params.product,
+      quantity: req.params.quantity,
+      totalPrice: req.params.totalPrice,
+      priceWithTax: req.params.priceWithTax,
+      status: req.params.status,
+    });
+    db.create(createCart)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
   update: function (req, res) {
-    db.findOneAndUpdate({ _id: req.params.id}, req.body)
+    let updateCart = new db({ _id: req.params.id }, req.body);
+    db.findOneAndUpdate(updateCart)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
-  updateAProduct: function (req, res) {
-    db.findOneAndUpdate({ _id: req.params.id, 'products._id': req.params.product }, req.body)
-      .then((dbModel) => res.json(dbModel))
-      .catch((err) => res.status(422).json(err));
-  },
+  // updateAProduct: function (req, res) {
+  //   let updateProduct = new db(
+  //     { _id: req.params.id, 'products._id': req.params.product },
+  //     req.body
+  //   );
+  //   db.findOneAndUpdate(updateProduct)
+  //     .then((dbModel) => res.json(dbModel))
+  //     .catch((err) => res.status(422).json(err));
+  // },
   remove: function (req, res) {
-    db.findById({ _id: req.params.id })
+    let removeProduct = new db({ _id: req.params.id });
+    db.findById(removeProduct)
       .then((dbModel) => dbModel.remove())
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
-  removeAProduct: function (req, res) {
-    db.findOneAndRemove({ _id: req.params.id, 'products._id': req.params.product })
-     // .then((dbModel) => dbModel.remove())
-      .then((dbModel) => {
-        console.log(res.json(dbModel));
-          res.json(dbModel)})
-      .catch((err) => res.status(422).json(err));
-  },
+  // removeAProduct: function (req, res) {
+  //   let removeProduct = new db({
+  //     _id: req.params.id,
+  //     'products._id': req.params.product,
+  //   });
+  //   db.findOneAndRemove(removeProduct)
+  //     // .then((dbModel) => dbModel.remove())
+  //     .then((dbModel) => {
+  //       console.log(res.json(dbModel)); // FOR TESTING
+  //       res.json(dbModel);
+  //     })
+  //     .catch((err) => res.status(422).json(err));
+  // },
 };
--- routes/api/cart.js
+++ routes/api/cart.js
@@ -8,36 +8,35 @@ router.route('/').get(cartController.findAll).post(cartController.create);
 router
   .route('/:id')
   .get(cartController.findById)
-  .put(cartController.update)
+  .put(cartController.update) // used in the cart page
   .delete(cartController.remove);
 
-  // Matches with cart id and product id
-router
-.route('/id/:id/:product')
-.get(cartController.findByIdandProduct)
-.put(cartController.updateAProduct)
-.delete(cartController.removeAProduct);
+// Matches with cart id and product id
+// router
+//   .route('/id/:id/:product')
+//   .get(cartController.findByIdandProduct)
+//   .put(cartController.updateAProduct)
+//   .delete(cartController.removeAProduct);
 
 // Matches with product id only
-router
-  .route('/product/:product')
-  .get(cartController.findByProduct)
-  .put(cartController.update)
-  .delete(cartController.remove);
+// router
+//   .route('/product/:product')
+//   .get(cartController.findByProduct)
+//   .put(cartController.update)
+//   .delete(cartController.remove);
 
 // Matches with "/status/:status"
+// router
+//   .route('/status/:status')
+//   .get(cartController.findByStatus)
+//   .put(cartController.update)
+//   .delete(cartController.remove);
+
+// Matches with user id and status
 router
-  .route('/status/:status')
-  .get(cartController.findByStatus)
+  .route('/:user/:status')
+  .get(cartController.findByUserandStatus) // used in the cart page
   .put(cartController.update)
   .delete(cartController.remove);
-
-  // Matches with user id and status
-router
-.route('/:user/:status')
-.get(cartController.findByUserandStatus)
-.put(cartController.update)
-.delete(cartController.remove);
-
 
 module.exports = router;
