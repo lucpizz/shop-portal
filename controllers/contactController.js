95e673ee49da087cfa344bca05716a5279db1bd1
lucpizz <Lucpizz@gmail.com>
2021-04-19 14:16:36 -0400

    updated contactController configuration

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,32 +3,23 @@ const db = require('../models/contactModel');
 // Defining methods for the postsController contact
 module.exports = {
   findAll: function (req, res) {
-    db.find()
+    db.find(req.query)
       .sort({ created: -1 })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
   findById: function (req, res) {
-    let findId = new db({
-      id: req.params.id,
-    });
-    db.findById(findId)
+    db.findById({ _id: req.params.id })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
   findByName: function (req, res) {
-    let findName = new db({
-      name: req.params.name,
-    });
-    db.findOne(findName)
+    db.findOne({ name: req.params.name })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
   findByEmail: function (req, res) {
-    let findEmail = new db({
-      email: req.params.email,
-    });
-    db.findOne(findEmail)
+    db.findOne({ email: req.params.email })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
